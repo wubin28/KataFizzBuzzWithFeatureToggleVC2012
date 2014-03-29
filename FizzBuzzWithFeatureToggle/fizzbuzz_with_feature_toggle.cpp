f3b3ba6b040cbaf72654e318e0825bce1867fd40
Ben Wu <wubin28@gmail.com>
2014-03-29 23:12:44 +0800

    Made TEST_F(FizzBuzzWithFeatureToggleTest, WhenMultipleOf3ThenFizz) passed.

--- FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle.cpp
+++ FizzBuzzWithFeatureToggle/fizzbuzz_with_feature_toggle.cpp
@@ -1,5 +1,8 @@
 #include "fizzbuzz_with_feature_toggle.h"
 
 string FizzBuzzHandler::handle(int number) {
+    if (number % 3 == 0) {
+        return "Fizz";
+    }
 	return std::to_string(number);
 }
